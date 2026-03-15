1984ef3db6207206fccec82a191447ecc626394b
112202407264 <112202407264@mhs.dinus.ac.id>
2026-03-15 22:23:34 +0700

    perbaikan header

--- include/header.php
+++ include/header.php
@@ -30,21 +30,64 @@ $cssVer = file_exists($cssFile) ? filemtime($cssFile) : time();
 </head>
 <body class="app-body">
 
-<header class="app-topbar">
-    <div class="app-topbar-inner">
-        <a class="app-topbar-brand" href="<?= $baseUrl ?>index.php">
-            <span class="app-topbar-icon">
-                <i class="fa-solid fa-shoe-prints"></i>
-            </span>
-            <span class="app-topbar-brand-text">
-                <span class="app-topbar-title">Inventaris Sepatu</span>
-                <span class="app-topbar-sub">Sistem Pendataan Barang</span>
-            </span>
-        </a>
-        <div class="app-topbar-right">
-            <div class="app-topbar-date">
-                <i class="fa-regular fa-calendar"></i>
-                <span><?= date('d/m/Y') ?></span>
+<?php
+$hour = (int) date('H');
+if ($hour >= 5 && $hour < 12) {
+    $greeting = 'Selamat pagi';
+} elseif ($hour >= 12 && $hour < 15) {
+    $greeting = 'Selamat siang';
+} elseif ($hour >= 15 && $hour < 18) {
+    $greeting = 'Selamat sore';
+} else {
+    $greeting = 'Selamat malam';
+}
+?>
+<header class="app-topbar shadow-sm">
+    <div class="app-topbar-inner container-fluid">
+        <div class="d-flex align-items-center gap-3">
+            <a class="app-topbar-brand" href="<?= $baseUrl ?>index.php">
+                <span class="app-topbar-icon">
+                    <i class="fa-solid fa-shoe-prints"></i>
+                </span>
+                <span class="app-topbar-brand-text">
+                    <span class="app-topbar-title">Inventaris Sepatu</span>
+                    <span class="app-topbar-sub">Sistem Pendataan Barang</span>
+                </span>
+            </a>
+            <div class="d-none d-md-flex flex-column small text-light-50">
+                <span><?= $greeting; ?>, selamat bekerja!</span>
+                <span>Pastikan data barang selalu terbarui.</span>
+            </div>
+        </div>
+
+        <div class="app-topbar-right d-flex align-items-center gap-3">
+            <form class="d-none d-lg-flex align-items-center" role="search" action="<?= $baseUrl ?>pages/data_barang.php" method="get">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-text bg-transparent border-end-0">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </span>
+                    <input type="text" class="form-control border-start-0" name="q" placeholder="Cari barang...">
+                </div>
+            </form>
+
+            <div class="dropdown">
+                <button class="btn btn-sm btn-outline-light d-flex align-items-center gap-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="avatar rounded-circle bg-light text-primary d-inline-flex align-items-center justify-content-center" style="width:28px;height:28px;">
+                        <i class="fa-solid fa-user"></i>
+                    </span>
+                    <span class="d-none d-sm-inline text-start">
+                        <span class="d-block lh-1 small">Administrator</span>
+                        <span class="d-block lh-1 text-light-50 extra-small">Inventaris</span>
+                    </span>
+                    <i class="fa-solid fa-chevron-down extra-small"></i>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end shadow-sm">
+                    <li class="dropdown-header small text-muted">Akun</li>
+                    <li><a class="dropdown-item" href="#"><i class="fa-solid fa-id-badge me-2"></i>Profil</a></li>
+                    <li><a class="dropdown-item" href="#"><i class="fa-solid fa-gear me-2"></i>Pengaturan</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item text-danger" href="#"><i class="fa-solid fa-right-from-bracket me-2"></i>Keluar</a></li>
+                </ul>
             </div>
         </div>
     </div>
